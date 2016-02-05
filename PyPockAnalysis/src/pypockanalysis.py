5a310706243e17f6a026225376ba7767df97d6c8
carajillu <joan.clark88@gmail.com>
2016-02-05 18:01:29 +0000

    functions split, f/mdpocket, hbond and docking working

--- PyPockAnalysis/src/pypockanalysis.py
+++ PyPockAnalysis/src/pypockanalysis.py
@@ -1,6 +1,7 @@
 import MDAnalysis as md
 from MDAnalysis import *
 from MDAnalysis.analysis.align import *
+import MDAnalysis.analysis.hbonds as hb
 import os, sys, math, numpy
 
 ###########################
@@ -69,6 +70,36 @@ def runMDPocket(pdbList):
     print command
     os.system(command)
     return 0
+
+def Fpocket_analysis(pdbList,trr):
+    # Initializing pocket properties file
+    propName=trr.split('.')[0]+'_druggability.txt'
+    propOut=open(propName,'w')
+    propOut.write('Pocket_Score Drug_Score Num_Vornoi Mean_alpha_radius Mean_alpha_SA Mean_Bf Hydrophob_score Polar_score Vol_score Real_Vol Char_score Loc_hydroph_dens_score Num_apol_spheres Prop_apol_sph\n')
+    # Initializing pocket coordinates file
+    pocketName=trr.split('.')[0]+'_pocket.pqr'
+    pocketOut=open(pocketName,'w')
+    i=0
+    for snapshot in pdbList:
+       props=[]
+       SnapPocketName=snapshot.split('.')[0]+'_out/pockets/pocket0_vert.pqr'
+       snapPocketIn=open(SnapPocketName,'r')
+       print 'Opened file '+SnapPocketName+' for analysis'
+       pocketOut.write('MODEL '+str(i)+'\n')
+       i=i+1
+       for line in snapPocketIn:
+           if 'HEADER' in line and ' - ' in line:
+               props.append((line.split(':')[1]).strip('\n').strip(' '))
+           elif 'ATOM' in line:
+               pocketOut.write(line)
+       pocketOut.write('TER\nENDMDL\n\n')
+       props=(' '.join(props))+'\n'
+       propOut.write(props)
+       snapPocketIn.close()
+    propOut.close()
+    pocketOut.close()
+    return 0
+
 
 #########################################
 # SECTION DOCKING: vina                       #
@@ -143,20 +174,93 @@ def vina_ensemble(pdbqtList,pockFrame,ligand):
     return 0
 
 #########################################
+# SECTION HBONDS                        #
+#########################################
+def HbondCalc(resOfInt,tpr,trr):
+    for residue in resOfInt:
+        myMD=md.Universe(tpr,trr)
+        resid='resid '+residue+' and not backbone'
+        print resid
+        h=hb.HydrogenBondAnalysis(myMD,resid, 'all', distance=3.0, angle=120.0)
+        h.run()
+        Hbonds=h.timeseries
+        nameOut='hbonds_'+residue+'.txt'
+        fileout=open(nameOut,'w')
+        hbList=[]
+        for frame in Hbonds:
+            line=[frame[0][2].split(':')[0]]
+            for bond in frame:
+                line.append(bond[3].split(':')[0])
+            fileout.write(' '.join(line)+'\n')
+    return 0
+         
+#########################################
+# SECTION SOLVATION                     #
+#########################################
+
+
+
+#########################################
 # MAIN FUNCITON                         #
 #########################################
 
 tpr=sys.argv[1]
 trr=sys.argv[2]
+ligand=sys.argv[3]
+resToIgnore=['SOL','NA','CL',ligand.split('.')[0]] # maybe you don't need to ignore the ligand...
 
-resToIgnore=['SOL','NA','CL',] # maybe you don't need to ignore the ligand...
+if '-split' in sys.argv:
+   nameAli=align(tpr,trr)
+   pdbList=splitPDB(tpr,nameAli,resToIgnore,'pdb')
+else:
+   frameList=[]
+   filein=open('splitPDB.txt','r')
+   for line in filein:
+      frameList.append(line.strip('\n'))
 
-nameAli=align(tpr,trr)
-pdbList=splitPDB(tpr,nameAli,resToIgnore,'pdb')
-runMDPocket(pdbList)
+if '-hbond' in sys.argv:
+    resOfInt=[]
+    entry=''
+    while entry!='STOP':
+        entry=raw_input('Enter a residue number (they begin at 0) or STOP to finish: ')
+        if entry!='STOP':
+            resOfInt.append(entry)  
+        HbondCalc(resOfInt,tpr,trr)
+
+if '-fpocket' in sys.argv:
+    runFPocket(pdbList)
+    Fpocket_analysis(pdbList,trr)
+
+if '-mdpocket' in sys.argv:
+    runMDPocket(pdbList)
+    
+if '-docking' in sys.argv:
+    if '-fpocket' not in sys.argv:
+       command='fpocket -f '+pdbList[0]
+       os.system(command)
+    pdbqtList=genPDBQT(pdbList)
+    vina_ensemble(pdbqtList,pdbList[0],ligand)
+    
+
+
+
+
+
+
+
+
+
+
+#HbondCalc(resOfInt,tpr,trr)
+
+#nameAli=align(tpr,trr)
+#pdbList=splitPDB(tpr,nameAli,resToIgnore,'pdb')
+#runFPocket(pdbList)
+#Fpocket_analysis(pdbList,trr)
+#runMDPocket(pdbList)
 
 #pdbqtList=splitPDB(tpr,nameAli,resToIgnore,'pdbqt')
-pdbqtList=genPDBQT(pdbList)
+#pdbqtList=genPDBQT(pdbList)
 '''
 for snapshot in mol2List:
     pdbqtName=snapshot.split('.')[0]+'.pdbqt'
@@ -165,5 +269,4 @@ for snapshot in mol2List:
                '-r '+snapshot+' -o '+pdbqtName
     pdbqtList.append(pdbqtName)'''
     
-ligand='SKF.pdbqt'
-vina_ensemble(pdbqtList,pdbList[0],ligand)
+#vina_ensemble(pdbqtList,pdbList[0],ligand)
